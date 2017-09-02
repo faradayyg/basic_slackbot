ad8dd1dcd6ba951f01d3619ae1ba4a32af156943
Faradayyg <faradayyg@gmail.com>
2017-09-02 03:26:59 +0100

    responding to events

--- app/SlackAPIs.py
+++ app/SlackAPIs.py
@@ -1,12 +1,23 @@
 from app import app, api, Resource
 from flask import request
 
+from slackclient import SlackClient 
+
+BOT_NAME = 'Uobot'
+
+slack_client = SlackClient('xoxb-236316908951-Fjnqv4KZARjLgulLcpbxZ7XO')
 
 class HelloWorld(Resource):
 	def get(self):
 		return {'hello' : 'world'}
 
 	def post(self):
+		slack_client.api_call(
+		  "chat.postMessage",
+		  channel="#bot_test",
+		  text="Hello from Python! :tada:"+str(request.json)
+		)
+
 		return request.json
 
 class ToDo(Resource):
